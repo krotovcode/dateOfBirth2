724055ae449e5167692449a7dd8cd2985a69a74a
krotovcode <youngspirits@yandex.ru>
2025-01-26 03:10:48 +0300

    New

--- dateOfBirth.sol
+++ dateOfBirth.sol
@@ -1,2 +1,34 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.26;
+
+
+contract birthsdays{
+
+    uint256 myFavouriteNumber;
+
+
+    struct Person {
+        string name;
+        string dateOfBirth;
+        string zodiac;
+    }
+
+    Person[] public birthsdayList;
+
+    mapping (string => string)  public nameToDayOfBirth;
+
+
+    function addPerson (string memory _name, string memory _birthday, string memory _zodiac) public {
+        birthsdayList.push(Person(_name, _birthday, _zodiac));
+        nameToDayOfBirth[_name] = _birthday;
+    }
+
+
+}
+
+
+
+
+
+
+
